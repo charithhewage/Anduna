4bb95a899c927beb1caf9a9d7958c427569525f3
Charith Hewage <charithhewage@gmail.com>
2015-11-25 23:01:36 +0530

    routes changed

--- config/initializers/utils.js
+++ config/initializers/utils.js
@@ -1,15 +1,4 @@
-var log = new Log();
-
-var toMD5 = function (val) {
-    var _md5 = require('../config/initializers/md5.js');
-    return _md5.MD5(val);
-};
-
-
-var current_user = function(){
-	var user = session.get('user');
-  if (!user) {
-      return null;
-  }
-  return user;
+var toMD5 = function(val){
+	var _md5 = require('../config/initializers/md5.js');
+  return _md5.MD5(val);
 }
