adcd0dc071a1e949f40333a9cc01284ee06dcc8e
Charith Hewage <charithhewage@gmail.com>
2015-11-26 13:16:42 +0530

    admin section modifications

--- config/initializers/utils.js
+++ config/initializers/utils.js
@@ -2,3 +2,14 @@ var toMD5 = function(val){
 	var _md5 = require('../config/initializers/md5.js');
   return _md5.MD5(val);
 }
+
+var relativePrefix = function (path) {
+    var parts = path.split('/');
+    var prefix = '';
+    var i;
+    var count = parts.length - 3;
+    for (i = 0; i < count; i++) {
+        prefix += '../';
+    }
+    return prefix;
+};
